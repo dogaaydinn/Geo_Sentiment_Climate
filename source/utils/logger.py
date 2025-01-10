e10ed78fc26ecb8f0c15d09026350d8c74205aef
doga <dogaa882@gmail.com>
2025-01-10 20:25:16 +0300

    Update logger.py with more advanced techniques.

--- source/utils/logger.py
+++ source/utils/logger.py
@@ -1,28 +1,50 @@
+# source/utils/logger.py
+
 import logging
+from pathlib import Path
+from typing import Optional
+import colorlog
+from logging.handlers import RotatingFileHandler
 
 
 def setup_logger(name: str, log_file: str, log_level=logging.INFO) -> logging.Logger:
+    """
+    Sets up a logger with both file and colored console handlers.
+
+    Args:
+        name (str): Name of the logger.
+        log_file (str): Path to the log file.
+        log_level (int, optional): Logging level. Defaults to logging.INFO.
+
+    Returns:
+        logging.Logger: Configured logger.
+    """
     logger = logging.getLogger(name)
     logger.setLevel(log_level)
+    logger.propagate = False  # Propagation'ı kapatın
 
-    # Formatter
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    if not logger.handlers:
+        # File handler with rotation
+        file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        fh = RotatingFileHandler(log_file, maxBytes=5 * 1024 * 1024, backupCount=5)
+        fh.setLevel(log_level)
+        fh.setFormatter(file_formatter)
+        logger.addHandler(fh)
 
-    # File handler
-    fh = logging.FileHandler(log_file)
-    fh.setLevel(log_level)
-    fh.setFormatter(formatter)
-
-    # Console handler
-    ch = logging.StreamHandler()
-    ch.setLevel(log_level)
-    # Create formatter and add it to the handlers
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
-    ch.setFormatter(formatter)
-
-    # Add the handlers to the logger
-    logger.addHandler(fh)
-    logger.addHandler(ch)
+        # Console handler with color
+        console_formatter = colorlog.ColoredFormatter(
+            "%(log_color)s%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            log_colors={
+                'DEBUG': 'cyan',
+                'INFO': 'green',
+                'WARNING': 'yellow',
+                'ERROR': 'red',
+                'CRITICAL': 'bold_red',
+            }
+        )
+        ch = logging.StreamHandler()
+        ch.setLevel(log_level)
+        ch.setFormatter(console_formatter)
+        logger.addHandler(ch)
 
     return logger
