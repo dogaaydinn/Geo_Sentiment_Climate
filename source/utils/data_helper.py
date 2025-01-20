f5b0281bbd12026e513a26d44a6579454ded93c7
doga <dogaa882@gmail.com>
2025-01-20 15:13:44 +0300

    Add error handling and logging to data_helper module

---
+++ source/utils/data_helper.py
@@ -0,0 +1,6 @@
+from source.data.data_preprocessing.base_data_processor import BaseDataProcessor
+
+class DataHelper(BaseDataProcessor):
+    def __init__(self, logger):
+        super().__init__(logger)
+
